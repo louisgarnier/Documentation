fe420e9e2e8aecf96197f02d01ee97153b17c2de
Louis <louisgarnierpro@gmail.com>
2025-11-18 20:06:11 +0100

    Remove test-case-manager folder - no longer needed

--- test-case-manager/types.ts
+++
@@ -1,9 +0,0 @@
-
-export interface TestCase {
-  id: string;
-  description: string;
-  createdDate: string;
-  steps: string[];
-  expectedResult: string;
-  status: 'Pass' | 'Fail' | 'Untested';
-}
